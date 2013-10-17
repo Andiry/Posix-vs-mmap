f0c02c4d82e400154311b4b5786b4688927c70a5
Andiry Xu <jix024@cs.ucsd.edu>
2013-10-17 15:09:01 -0700

    Size loop for write_to_ram

--- write_to_ram.c
+++ write_to_ram.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<time.h>
+#include<string.h>
+#include<malloc.h>
+#include<stdlib.h>
+#include<sys/mman.h>
 
 #define SIZE	(4 * 1024 * 1024) 
 
@@ -8,18 +12,37 @@ int main(void)
 {
 	int fd, i;
 	unsigned long time;
-	char c = 'x';
+	char c = 'a';
 	struct timespec start, end;
+	int start_size = 1, count;
+	void *buf1 = NULL;
+	char *buf;
 
-	fd = open("/mnt/ramdisk/test1", O_CREAT | O_RDWR); 
+	posix_memalign(&buf1, SIZE, SIZE); // up to 4MB, same as ram disk
+	if (!buf1) {
+		printf("ERROR!\n");
+		return 0;
+	}
 
-	clock_gettime(CLOCK_MONOTONIC, &start);
-	for (i = 0; i < SIZE; i++)
-		write(fd, &c, 1);
+	buf = (char *)buf1;
 
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
-	printf("Used %ld nanoseconds.\n", time);
-	close(fd);
+	for (start_size = 1; start_size <= SIZE; start_size <<= 1) {
+		memset(buf, c, start_size);
+		c++;
+
+		fd = open("/mnt/ramdisk/test1", O_CREAT | O_RDWR); 
+
+		count = SIZE / start_size;
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for (i = 0; i < count; i++)
+			write(fd, buf, start_size);
+
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+		printf("Size %d bytes,\t %ld nanoseconds,\t Bandwidth %f MB/s.\n", start_size, time, 4.0 * 1e9 / time);
+		close(fd);
+	}
+
+	free(buf1);
 	return 0;
 }
