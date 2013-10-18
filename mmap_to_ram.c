a063952047358a5c781bb2350a981a8da7d4139b
Andiry Xu <jix024@cs.ucsd.edu>
2013-10-17 17:08:11 -0700

    loop size for mmap_to_ram

--- mmap_to_ram.c
+++ mmap_to_ram.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
 #include<fcntl.h>
 #include<time.h>
 #include<sys/mman.h>
@@ -10,23 +12,42 @@ int main(void)
 {
 	int fd, i;
 	unsigned long time;
-	char c = 'b';
+	char c = 'a';
 	char *data = NULL, *origin_data;
 	struct timespec start, end;
+	void *buf1 = NULL;
+	char *buf;
+	int start_size, count;
 
-	fd = open("/mnt/ramdisk/test2", O_CREAT | O_RDWR); 
-	data = (char *)mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
-	origin_data = data;
+	posix_memalign(&buf1, SIZE, SIZE);
 
-	clock_gettime(CLOCK_MONOTONIC, &start);
-	for (i = 0; i < SIZE; i++)
-		memcpy(data++, &c, 1);
+	buf = (char *)buf1;
 
-	msync(origin_data, SIZE, MS_SYNC);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
-	printf("Mmap: Used %ld nanoseconds.\n", time);
-	munmap(origin_data, SIZE);
-	close(fd);
+	for (start_size = 1; start_size <= SIZE; start_size <<= 1) {
+		buf = (char *)buf1;
+		memset(buf, c, start_size);
+		c++;
+
+		fd = open("/mnt/ramdisk/test2", O_CREAT | O_RDWR); 
+		data = (char *)mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+		origin_data = data;
+
+		count = SIZE / start_size;
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for (i = 0; i < count; i++) {
+			memcpy(data, buf, start_size);
+			data += start_size;
+			buf += start_size;
+		}
+
+		msync(origin_data, SIZE, MS_ASYNC);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+		printf("Mmap: size %d bytes, %d times,\t %ld nanoseconds,\t Bandwidth %f MB/s.\n", start_size, count, time, 4.0 * 1e9 / time);
+		munmap(origin_data, SIZE);
+		close(fd);
+	}
+
+	free(buf1);
 	return 0;
 }
