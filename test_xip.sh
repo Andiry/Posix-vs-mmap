14b9eaf5968f9e267a8bac00d5662d303a088e15
Andiry Xu <jix024@cs.ucsd.edu>
2013-10-23 16:01:15 -0700

    Add xip support

--- test_xip.sh
+++ test_xip.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Make ext2 on /dev/ram1.."
-mkfs.ext2  /dev/ram0
+mkfs.ext2 -b 4096 /dev/ram0
 
 mkdir /mnt/ramdisk
 
-mount -t ext2  /dev/ram0 /mnt/ramdisk
+mount -t ext2 -o xip /dev/ram0 /mnt/ramdisk
 
 echo "Mount to /mnt/ramdisk."
