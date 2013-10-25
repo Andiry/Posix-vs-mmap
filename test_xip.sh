054528be1b738ccdb95f0177ba3f65b83f5bd097
Andiry Xu <jix024@cs.ucsd.edu>
2013-10-25 12:28:48 -0700

    Modify test_xip.sh on bb cluster

--- test_xip.sh
+++ test_xip.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "Make ext2 on /dev/ram1.."
-mkfs.ext2 -b 4096 /dev/ram0
+sudo mke2fs -b 4096 /dev/memuram0
+sudo chmod a+rw /dev/memuram0
 
 mkdir /mnt/ramdisk
 
-mount -t ext2 -o xip /dev/ram0 /mnt/ramdisk
+sudo mount -t ext2 -o xip /dev/memuram0 /mnt/ramdisk
+sudo chmod a+rw /mnt/ramdisk
 
 echo "Mount to /mnt/ramdisk."
