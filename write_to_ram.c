37d9651d610da9bb1b0a92c1aa02515bce61064d
Andiry Xu <jix024@cs.ucsd.edu>
2013-10-28 13:41:23 -0700

    Optimize write_to_ram (open file only once)

--- write_to_ram.c
+++ write_to_ram.c
@@ -30,12 +30,12 @@ int main(void)
 	}
 
 	buf = (char *)buf1;
+	fd = open("/mnt/ramdisk/test1", O_CREAT | O_RDWR | O_DIRECT); 
 
 	for (size = start_size; size <= END_SIZE; size <<= 1) {
 		memset(buf, c, size);
 		c++;
-
-		fd = open("/mnt/ramdisk/test1", O_CREAT | O_RDWR | O_DIRECT); 
+		lseek(fd, 0, SEEK_SET);
 
 		count = FILE_SIZE / size;
 		clock_gettime(CLOCK_MONOTONIC, &start);
@@ -45,9 +45,9 @@ int main(void)
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
 		printf("Size %d bytes,\t %d times,\t %ld nanoseconds,\t Bandwidth %f MB/s.\n", size, count, time, FILE_SIZE * 1024.0 / time);
-		close(fd);
 	}
 
+	close(fd);
 	free(buf1);
 	return 0;
 }
