1f261eb634c86ef725663dfdda3bfeeffd70b943
Andiry Xu <jix024@cs.ucsd.edu>
2013-11-08 14:45:36 -0800

    Add two optimized memcpy to mmap_to_ram

--- mmap_to_ram.c
+++ mmap_to_ram.c
@@ -14,6 +14,96 @@
 
 const int start_size = 512;
 
+char* memcpy1(char *to, char *from, size_t n)
+{
+	long esi, edi;
+	int ecx;
+	esi = (long)from;
+	edi = (long)to;
+	asm volatile("rep ; movsl"
+		: "=&c" (ecx), "=&D" (edi), "=&S" (esi)
+		: "0" (n / 4), "1" (edi), "2" (esi)
+		: "memory"
+		);
+	return to;
+}
+
+#define MMX2_MEMCPY_MIN_LEN 0x40
+#define MMX_MMREG_SIZE 8
+
+// ftp://ftp.work.acer-euro.com/gpl/AS1800/xine-lib/src/xine-utils/memcpy.c
+static void * mmx2_memcpy(void * __restrict__ to, const void * __restrict__ from, size_t len)
+{
+  void *retval;
+  size_t i;
+  retval = to;
+
+  /* PREFETCH has effect even for MOVSB instruction ;) */
+  __asm__ __volatile__ (
+    "   prefetchnta (%0)\n"
+    "   prefetchnta 32(%0)\n"
+    "   prefetchnta 64(%0)\n"
+    "   prefetchnta 96(%0)\n"
+    "   prefetchnta 128(%0)\n"
+    "   prefetchnta 160(%0)\n"
+    "   prefetchnta 192(%0)\n"
+    "   prefetchnta 224(%0)\n"
+    "   prefetchnta 256(%0)\n"
+    "   prefetchnta 288(%0)\n"
+    : : "r" (from) );
+
+  if(len >= MMX2_MEMCPY_MIN_LEN)
+  {
+    register unsigned long int delta;
+    /* Align destinition to MMREG_SIZE -boundary */
+    delta = ((unsigned long int)to)&(MMX_MMREG_SIZE-1);
+    if(delta)
+    {
+      delta=MMX_MMREG_SIZE-delta;
+      len -= delta;
+      memcpy(to, from, delta);
+    }
+    i = len >> 6; /* len/64 */
+    len&=63;
+    for(; i>0; i--)
+    {
+      __asm__ __volatile__ (
+      "prefetchnta 320(%0)\n"
+      "prefetchnta 352(%0)\n"
+      "movq (%0), %%mm0\n"
+      "movq 8(%0), %%mm1\n"
+      "movq 16(%0), %%mm2\n"
+      "movq 24(%0), %%mm3\n"
+      "movq 32(%0), %%mm4\n"
+      "movq 40(%0), %%mm5\n"
+      "movq 48(%0), %%mm6\n"
+      "movq 56(%0), %%mm7\n"
+      "movntq %%mm0, (%1)\n"
+      "movntq %%mm1, 8(%1)\n"
+      "movntq %%mm2, 16(%1)\n"
+      "movntq %%mm3, 24(%1)\n"
+      "movntq %%mm4, 32(%1)\n"
+      "movntq %%mm5, 40(%1)\n"
+      "movntq %%mm6, 48(%1)\n"
+      "movntq %%mm7, 56(%1)\n"
+      :: "r" (from), "r" (to) : "memory");
+      //((const unsigned char *)from)+=64;
+      //((unsigned char *)to)+=64;
+      from = (void*)(((const unsigned char *)from) + 64);
+      to = (void*)(((unsigned char *)to) + 64);
+    }
+     /* since movntq is weakly-ordered, a "sfence"
+     * is needed to become ordered again. */
+    __asm__ __volatile__ ("sfence":::"memory");
+    __asm__ __volatile__ ("emms":::"memory");
+  }
+  /*
+   *	Now do the tail of the block
+   */
+  if(len) memcpy(to, from, len);
+  return retval;
+}
+
 int main(int argc, char ** argv)
 {
 	int fd, i;
@@ -103,7 +193,7 @@ int main(int argc, char ** argv)
 
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		for (i = 0; i < count; i++) {
-			memcpy(data, buf, size);
+			mmx2_memcpy(data, buf, size);
 			data += size;
 		}
 
