3196a5cd19a9c9446b034d40effc233e7fbf6500
Andiry Xu <jix024@cs.ucsd.edu>
2014-01-04 21:43:40 -0800

    Ext2 setup script on bb cluster fix

--- clean-bb.sh
+++ clean-bb.sh
@@ -6,8 +6,8 @@ sudo rmmod memudisk
 
 sleep 1
 
-#RD_SIZE=2097152
-RD_SIZE=44040192
+RD_SIZE=2097152
+#RD_SIZE=44040192
 echo Mount memudisk with $RD_SIZE KB...
 QUIET=/dev/null
 
@@ -18,6 +18,7 @@ sleep 1
 
 sudo chmod a+rw /dev/memuram0
 echo Fill the memudisk...
-dd if=/dev/zero of=/dev/memuram0 bs=1M count=43008 oflag=direct
+#dd if=/dev/zero of=/dev/memuram0 bs=1M count=43008 oflag=direct
+dd if=/dev/zero of=/dev/memuram0 bs=1M count=1024 oflag=direct
 sleep 1
 
