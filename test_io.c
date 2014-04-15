bde4ab012e8f3bb3911e6dc9f2e5107a8d555f6b
Andiry Xu <jix024@cs.ucsd.edu>
2014-04-14 23:15:52 -0700

    test_io more cases

--- test_io.c
+++ test_io.c
@@ -11,11 +11,13 @@
 int main(void)
 {
 	int fd;
-	char *buf;
+	char *buf, *buf1;
 	ssize_t ret;
 
 	buf = malloc(4096 * 6);
-	memset(buf, 'c', 4096 * 6);
+	memset(buf, 'd', 4096 * 6);
+	buf1 = malloc(4096 * 6);
+	memset(buf1, 'a', 4096 * 6);
 
 	fd = open("/mnt/ramdisk/test1", O_RDWR | O_CREAT, 0640);
 
@@ -23,24 +25,28 @@ int main(void)
 	ret = pwrite(fd, buf, 4096 * 6, 0);
 	printf("Posix write to file: 0 - 5 ret: %d\n", ret);
 
-	printf("Posix read to file: 2 - 4\n");
-	ret = pread(fd, buf, 4096 * 3, 4096 * 2);
-	printf("Posix read to file: 2 - 4 ret: %d\n", ret);
+	printf("Posix read from file: 2 - 4\n");
+	ret = pread(fd, buf1, 4096 * 3, 4096 * 2);
+	printf("Posix read from file: 2 - 4 ret: %d, buf: %c\n", ret, buf1[0]);
 
+	memset(buf, 'f', 4096 * 6);
 	printf("Posix write to file: 1 - 3\n");
 	ret = pwrite(fd, buf, 4096 * 3, 4096);
 	printf("Posix write to file: 1 - 3 ret: %d\n", ret);
 
+	memset(buf, 'e', 4096 * 6);
 	printf("Posix write to file: 4 - 8\n");
 	ret = pwrite(fd, buf, 4096 * 5, 4096 * 4);
 	printf("Posix write to file: 4 - 8 ret: %d\n", ret);
 
-	printf("Posix read to file: 2 - 5\n");
-	ret = pread(fd, buf, 4096 * 4, 4096 * 2);
-	printf("Posix read to file: 2 - 5 ret: %d\n", ret);
+	printf("Posix read from file: 2 - 5\n");
+	ret = pread(fd, buf1, 4096 * 4, 4096 * 2);
+	printf("Posix read from file: 2 - 5 ret: %d, buf: %c %c %c %c\n",
+		ret, buf1[0], buf1[4096], buf1[8192], buf1[4096 * 3]);
 
 	close(fd);
 	free(buf);
+	free(buf1);
 
 	return 0;
 }
