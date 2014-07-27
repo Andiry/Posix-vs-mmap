017a7dfdd753fc78b91cd2d60272fee768cf5ec2
Andiry Xu <jix024@cs.ucsd.edu>
2014-07-26 23:39:57 -0700

    Add timing for test_fallocate

--- test_fallocate.c
+++ test_fallocate.c
@@ -19,14 +19,21 @@ int main(void)
 	int ret;
 	int c = 32;
 	size_t size;
+	struct timespec start, end;
+	unsigned long time;
 
 	fd = open("/mnt/ramdisk/test2", O_CREAT | O_RDWR, 0640);
 
 	size = write(fd, &c, 4);
 	printf("%d %u\n", fd, size);
-	ret = fallocate(fd, 0, 0, 10485760);
 
-	printf("%d\n", ret);
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	ret = fallocate(fd, 0, 0, 1048576);
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	time = (end.tv_sec - start.tv_sec) * 1e9 + end.tv_nsec - start.tv_nsec;
+
+	printf("%d, %lu nanoseconds\n", ret, time);
 	close(fd);
 	return ret;
 }
