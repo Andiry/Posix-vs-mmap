40df07a700967c763a96e2593d070566ebbfd44b
Andiry Xu <jix024@cs.ucsd.edu>
2014-09-18 20:16:53 -0700

    write_to_ram refine

--- write_to_ram.c
+++ write_to_ram.c
@@ -35,15 +35,39 @@ int main(int argc, char **argv)
 	char quill_enabled[40];
 	char file_size_num[20];
 	char filename[60];
-	int enable_ftrace;
+	char request_size_num[20];
 
 	if (argc < 6) {
-		printf("Usage: ./write_to_ram $FS $SCNEARIO $ENABLE_FTRACE $FILE_SIZE $filename\n");
+		printf("Usage: ./write_to_ram $FS $SCNEARIO $REQUEST_SIZE $FILE_SIZE $filename\n");
 		return 0;
 	}
 
 	strcpy(fs_type, argv[1]);
 	strcpy(quill_enabled, argv[2]);
+
+	strcpy(request_size_num, argv[3]);
+	len = strlen(request_size_num);
+	unit = request_size_num[len - 1];
+	request_size_num[len - 1] = '\0';
+	size = atoll(request_size_num);
+	switch (unit) {
+	case 'K':
+	case 'k':
+		size *= 1024;
+		break;
+	case 'M':
+	case 'm':
+		size *= 1048576;
+		break;
+	case 'G':
+	case 'g':
+		size *= 1073741824;
+		break;
+	default:
+		printf("ERROR: REQUEST_SIZE should be #K/M/G format.\n");
+		return 0;
+		break;
+	}
 
 	strcpy(file_size_num, argv[4]);
 	len = strlen(file_size_num);
@@ -83,13 +107,13 @@ int main(int argc, char **argv)
 	}
 
 	buf = (char *)buf1;
-	fd = open("/root/test1/test1", O_CREAT | O_RDWR, 0640); 
+	system("rm -rf /mnt/ramdisk/test1");
+	system("./../bankshot2/kernel/clear_cache");
+	fd = open("/mnt/ramdisk/test1", O_CREAT | O_RDWR, 0640); 
 //	fd = open("/dev/null", O_WRONLY, 0640); 
 //	fd = open("/dev/zero", O_RDONLY, 0640); 
 	printf("fd: %d\n", fd);
-//	start_size = atoi(argv[2]);
-	enable_ftrace = atoi(argv[3]);
-	for (size = start_size; size <= END_SIZE; size <<= 1) {
+//	for (size = start_size; size <= END_SIZE; size <<= 1) {
 //		size = 4096 * 256;
 //		size = atoi(argv[2]);
 		memset(buf, c, size);
@@ -117,8 +141,8 @@ int main(int argc, char **argv)
 		lseek(fd, 0, SEEK_SET);
 		count = FILE_SIZE / size;
 		offset = 0;
-		if (enable_ftrace)
-			system("echo 1 > /sys/kernel/debug/tracing/tracing_on");
+//		if (enable_ftrace)
+//			system("echo 1 > /sys/kernel/debug/tracing/tracing_on");
 	
 		gettimeofday(&begin, &tz);
 		clock_gettime(CLOCK_MONOTONIC, &start);
@@ -132,14 +156,14 @@ int main(int argc, char **argv)
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		gettimeofday(&finish, &tz);
 
-		if (enable_ftrace)
-			system("echo 0 > /sys/kernel/debug/tracing/tracing_on");
+//		if (enable_ftrace)
+//			system("echo 0 > /sys/kernel/debug/tracing/tracing_on");
 		time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
 		time1 = (finish.tv_sec - begin.tv_sec) * 1e6 + (finish.tv_usec - begin.tv_usec);
 		printf("Write: Size %d bytes,\t %lld times,\t %lld nanoseconds,\t latency %lld nanoseconds, \t Bandwidth %f MB/s.\n", size, count, time, time / count, FILE_SIZE * 1024.0 / time);
 		printf("Write process %lld microseconds\n", time1);
 		fprintf(output, "%s,%s,%d,%lld,%lld,%lld,%f,%lld\n", fs_type, quill_enabled, size, FILE_SIZE, count, time, FILE_SIZE * 1.0 / time, time / count);
-	}
+//	}
 
 	fclose(output);
 	close(fd);
