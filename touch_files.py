c8edf6e35d0442856e557fa6b9957dd2a0df8ff1
Andiry Xu <jix024@cs.ucsd.edu>
2015-04-22 14:05:05 -0700

    Add touch_files.py

--- touch_files.py
+++ touch_files.py
@@ -5,19 +5,16 @@ import sys
 import os
 
 def main():
-	if len(sys.argv) < 3:
-		print "paramemter error: #num #size"
+	if len(sys.argv) < 2:
+		print "paramemter error: #num"
 		sys.exit(0)
 	os.system("mkdir /mnt/ramdisk/dir")
 	dir = "/mnt/ramdisk/dir/log000"
 	num = int(sys.argv[1])
-	size = int(sys.argv[2])
 	
 	for i in range(num):
 		filename = dir + str(i)
-		fd1 = open(filename, 'w')
-		fd1.write(size * 'a')
-		fd1.close()
+		os.system("touch " + filename)
 	print "done."
 	return
 
